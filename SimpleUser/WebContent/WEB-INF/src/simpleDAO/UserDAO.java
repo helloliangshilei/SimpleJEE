5aae14c872485dc532db8b681198b450247edaa7
Jason McKerr <mckerrj@gmail.com>
2013-01-24 14:06:19 -0500

    Exceptions, I think

--- SimpleUser/WebContent/WEB-INF/src/simpleDAO/UserDAO.java
+++ SimpleUser/WebContent/WEB-INF/src/simpleDAO/UserDAO.java
@@ -9,7 +9,7 @@ public interface UserDAO {
 	public void saveUser(User user) throws Exception;
 	public void changeUser(User user) throws Exception;
 	public User getUser(String username) throws Exception;
-	public void removeUser(User user) throws Exception;
+	public int removeUser(User user) throws Exception;
 	public List<User> listAllUsers() throws Exception;
 
 }
