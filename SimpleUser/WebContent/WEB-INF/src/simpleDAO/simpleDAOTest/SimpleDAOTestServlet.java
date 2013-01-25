d8f9d83427cec759a2a9137fd5d07388cf9d86f7
Jason McKerr <mckerrj@gmail.com>
2013-01-25 10:49:41 -0500

    Initial commit of first test for SimpleDAO stuff

---
+++ SimpleUser/WebContent/WEB-INF/src/simpleDAO/simpleDAOTest/SimpleDAOTestServlet.java
@@ -0,0 +1,38 @@
+package simpleDAO.simpleDAOTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import object.User;
+
+import org.apache.cactus.ServletTestCase;
+
+import commonDAO.UserDAOFactory;
+
+import simpleDAO.UserDAO;
+
+public class SimpleDAOTestServlet extends ServletTestCase {
+	
+	public SimpleDAOTestServlet(String theName) {
+		super(theName);
+	}
+
+	public static Test suite() {
+		return new TestSuite(SimpleDAOTestServlet.class);
+	}
+	
+	public void testUserGet() {
+		UserDAO userDAOGet = UserDAOFactory.getUserDAO("jdbc");
+		User userGet = new User();
+		try {
+			userGet = userDAOGet.getUser("mckerrj");
+		}
+		catch (Exception e) {
+			fail("Test failed with exception: " + e);
+		}
+		assertEquals("mckerrj", userGet.getUserName());
+		assertEquals("Jason", userGet.getFirstName());
+		assertEquals("McKerr", userGet.getLastName());
+		assertEquals("mckerrj314", userGet.getPassword());
+	}
+}
