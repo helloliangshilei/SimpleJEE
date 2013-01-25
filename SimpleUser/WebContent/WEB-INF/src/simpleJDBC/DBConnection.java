676c6a701ff98e45b0847b5e6aecbf6ec935ac05
Jason McKerr <mckerrj@gmail.com>
2013-01-25 12:06:46 -0500

    added logging

--- SimpleUser/WebContent/WEB-INF/src/simpleJDBC/DBConnection.java
+++ SimpleUser/WebContent/WEB-INF/src/simpleJDBC/DBConnection.java
@@ -10,10 +10,17 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import simpleDAO.UserDAOImplSimple;
+
 public class DBConnection {
 	
+	private static Logger log = LoggerFactory.getLogger(DBConnection.class);
+	
 	public static Connection getConnection() throws NamingException, SQLException {
-		//Abstract to resource file
+		//TODO Abstract to resource file
 		String dsContext = "java:comp/env/jdbc/simpleapp";
 		Connection conn = null;
 		Context initialContext = new InitialContext();  
@@ -23,7 +30,7 @@ public class DBConnection {
 			conn = datasource.getConnection();
 		}
 		catch (Exception e) {
-			System.out.println(e);
+			log.error("Exception happened in DBConnection: " + e);
 		}
 		return conn;
 	}
