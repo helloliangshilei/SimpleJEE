4ec56b600f2cfb84301668bba112d4f0129c49b7
Jason McKerr <mckerrj@gmail.com>
2013-01-26 11:39:01 -0500

    adding full constructor to play around with hibernate

--- SimpleUser/WebContent/WEB-INF/classes/object/User.java
+++ SimpleUser/WebContent/WEB-INF/classes/object/User.java
@@ -12,6 +12,13 @@ public class User implements Serializable {
 	public User() {
 	}
 	
+	public User(String userName, String firstName, String lastName, String password) {
+	  this.userName = userName;
+	  this.firstName = firstName;
+	  this.lastName = lastName;
+	  this.password = password;
+  }
+
 	public String getUserName() {
 		return this.userName;
 	}
