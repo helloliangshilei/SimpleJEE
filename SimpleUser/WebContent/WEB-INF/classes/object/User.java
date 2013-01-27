cd0f642f746f263bcdbe571b949896b83844a360
Jason McKerr <mckerrj@gmail.com>
2013-01-27 15:33:23 -0500

    added sets for roles

--- SimpleUser/WebContent/WEB-INF/classes/object/User.java
+++ SimpleUser/WebContent/WEB-INF/classes/object/User.java
@@ -1,28 +1,32 @@
 package object;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 public class User implements Serializable {
-	
+
 	private String userName = null;
 	private String firstName = null;
 	private String lastName = null;
 	private String password = null;
-	
+	private Set<Role> roles = new HashSet<Role>(0);
+
 	public User() {
 	}
-	
-	public User(String userName, String firstName, String lastName, String password) {
-	  this.userName = userName;
-	  this.firstName = firstName;
-	  this.lastName = lastName;
-	  this.password = password;
-  }
+
+	public User(String userName, String firstName, String lastName,
+			String password) {
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+	}
 
 	public String getUserName() {
 		return this.userName;
 	}
-	
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
@@ -49,5 +53,13 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return this.roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
 	}
 }
