b2aa2abed62e9dd18e636b61730e65f5bbd6f54a
Jason McKerr <mckerrj@gmail.com>
2013-01-28 11:22:14 -0500

    Working and sorta tested HibernateUtility.  Tested in that it works with the Thread manager in Hibernate and loads properly.  Nothing else tested yet.

---
+++ SimpleUser/WebContent/WEB-INF/classes/DAO/hibernateDAO/HibernateUtility.java
@@ -0,0 +1,59 @@
+package DAO.hibernateDAO;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import DAO.simpleDAO.UserDAOImplSimple;
+
+public class HibernateUtility {
+
+	private static final SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
+	
+	private static Logger log = LoggerFactory.getLogger(UserDAOImplSimple.class);
+
+	static {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		} 
+		catch (ExceptionInInitializerError eiie) {
+			log.error("Initial SessionFactory creation failed." + eiie);
+			throw new ExceptionInInitializerError(eiie);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+			return sessionFactory;
+	}
+
+	public static Session BeginTx() {
+		Session hibernateSession = HibernateUtility.getSession();
+		hibernateSession.beginTransaction();
+		return hibernateSession;
+	}
+
+	public static void commitTx() {
+		HibernateUtility.getSession().getTransaction().commit();
+	}
+
+	public static void rollBack() {
+		HibernateUtility.getSession().getTransaction().rollback();
+	}
+
+	public static void closeSession() {
+		HibernateUtility.getSession().close();
+	}
+
+	public static Session getSession() {
+		Session hibernateSession = sessionFactory.getCurrentSession();
+		return hibernateSession;
+	}
+}
