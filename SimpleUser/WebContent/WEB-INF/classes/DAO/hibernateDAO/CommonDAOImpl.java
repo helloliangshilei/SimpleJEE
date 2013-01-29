cb438d1a3c70dd88312e394708fca8bcf4f26141
Jason McKerr <mckerrj@gmail.com>
2013-01-29 18:02:06 -0500

    Move all TX logic out to correct data object.

--- SimpleUser/WebContent/WEB-INF/classes/DAO/hibernateDAO/CommonDAOImpl.java
+++ SimpleUser/WebContent/WEB-INF/classes/DAO/hibernateDAO/CommonDAOImpl.java
@@ -1,14 +1,14 @@
 package DAO.hibernateDAO;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
-//TODO OK, this all works basically.  No need to move all Tx logic to a lower level impl class.
-public abstract class CommonDAOImpl<T extends Serializable> implements CommonDAO<T> {
+
+
+public abstract class CommonDAOImpl<T extends Serializable> implements
+		CommonDAO<T> {
 
 	protected Session getSession() {
 		return HibernateUtility.getSession();
@@ -16,84 +16,38 @@ public abstract class CommonDAOImpl<T extends Serializable> implements CommonDAO
 
 	@Override
 	public void save(T entity) {
-		Session session = null;
-		Transaction tx = null;
-		try {
-			session = this.getSession();
-			tx = session.beginTransaction();
-			session.save(entity);
-			tx.commit();
-		}
-		catch (Exception e) {
-			tx.rollback();
-		}
+		Session session = this.getSession();
+		session.save(entity);
 	}
 
 	@Override
 	public void update(T entity) {
-		Session session = null;
-		Transaction tx = null;
-		try {
-			session = this.getSession();
-			tx = session.beginTransaction();
-			session.saveOrUpdate(entity);
-			tx.commit();
-		}
-		catch (Exception e) {
-			tx.rollback();
-		}
+		Session session = this.getSession();
+		session.saveOrUpdate(entity);
 	}
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public T find(Class<?> classy, String id) {
-		Session session = null;
-		Transaction tx = null;
-		T t = null;
-		try {
-			session = this.getSession();
-			tx = session.beginTransaction();
-			t = (T) session.get(classy, id);
-			tx.commit();
-		}
-		catch (Exception e) {
-			tx.rollback();
-		}
+		T t;
+		Session session = this.getSession();
+		t = (T) session.get(classy, id);
 		return t;
 	}
 
 	@Override
 	public void delete(T entity) {
-		Session session = null;
-		Transaction tx = null;
-		try {
-			session = this.getSession();
-			tx = session.beginTransaction();
-			session.delete(entity);
-			tx.commit();
-		}
-		catch (Exception e) {
-			tx.rollback();
-		}
+		Session session = this.getSession();
+		session.delete(entity);
 	}
 
-	@Override
 	@SuppressWarnings("unchecked")
+	@Override
 	public List<T> findAll(Class<?> classy) {
-		Session session = null;
-		Transaction tx = null;
-		List<T> list = new ArrayList<T>();
-		try {
-			session = this.getSession();
-			tx = session.beginTransaction();
-			Query queryResult = session.createQuery("from " + classy.getName());
-			//tx.commit();
-			list = queryResult.list();
-		}
-		catch (Exception e) {
-			//tx.rollback();
-			e.printStackTrace();
-		}
+		Session session = this.getSession();
+		List<T> list = null;
+		Query queryResult = session.createQuery("from " + classy.getName());
+		list = queryResult.list();
 		return list;
 	}
 }
