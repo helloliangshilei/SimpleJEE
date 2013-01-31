2b97cfc12353d1146087e94a80bfc81ef5bd5ec2
Jason McKerr <mckerrj@gmail.com>
2013-01-31 12:17:16 -0500

    adding logic to change from Hibernate mapped usage to Hibernate JPA Annotations style usage based on configuration

--- SimpleUser/WebContent/WEB-INF/classes/DAO/hibernateDAO/HibUtil.java
+++ SimpleUser/WebContent/WEB-INF/classes/DAO/hibernateDAO/HibUtil.java
@@ -1,6 +1,7 @@
 package DAO.hibernateDAO;
 
 import org.hibernate.Session;
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
@@ -8,20 +9,30 @@ import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import config.ConfigurationInit;
+
 import DAO.simpleDAO.UserDAOImplSimple;
 
 public class HibUtil {
-
+	
+	private static String hibernateConfigFile = ConfigurationInit.getHibernateConfigFile();
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 	private static ServiceRegistry serviceRegistry;
 	
 	private static Logger log = LoggerFactory.getLogger(UserDAOImplSimple.class);
 
 	private static SessionFactory buildSessionFactory() {
+		Configuration configuration = new Configuration();
 		try {
-			Configuration configuration = new Configuration();
-			configuration.configure();
-			serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+			if (!hibernateConfigFile.isEmpty() && hibernateConfigFile.equals("hibernateAnnotations.cfg.xml")) {
+				configuration = new Configuration();
+				configuration.configure(hibernateConfigFile);
+			}
+			else {
+				configuration = new Configuration();
+				configuration.configure();
+			}
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
 			return configuration.buildSessionFactory(serviceRegistry);
 		} 
 		catch (ExceptionInInitializerError eiie) {
