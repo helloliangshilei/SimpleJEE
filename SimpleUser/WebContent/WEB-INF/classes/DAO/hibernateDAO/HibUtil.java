c826facf7eb31be15ea45d55f567b1d13bc0157e
Jason McKerr <mckerrj@gmail.com>
2013-01-30 21:49:28 -0500

    change name for brevity

---
+++ SimpleUser/WebContent/WEB-INF/classes/DAO/hibernateDAO/HibUtil.java
@@ -0,0 +1,59 @@
+package DAO.hibernateDAO;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import DAO.simpleDAO.UserDAOImplSimple;
+
+public class HibUtil {
+
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+	private static ServiceRegistry serviceRegistry;
+	
+	private static Logger log = LoggerFactory.getLogger(UserDAOImplSimple.class);
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+			return configuration.buildSessionFactory(serviceRegistry);
+		} 
+		catch (ExceptionInInitializerError eiie) {
+			log.error("Initial SessionFactory creation failed." + eiie);
+			throw new ExceptionInInitializerError(eiie);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+			return sessionFactory;
+	}
+
+	public static Session beginTx() {
+		Session hibernateSession = HibUtil.getSession();
+		hibernateSession.beginTransaction();
+		return hibernateSession;
+	}
+
+	public static void commitTx() {
+		HibUtil.getSession().getTransaction().commit();
+	}
+
+	public static void rollBackTx() {
+		HibUtil.getSession().getTransaction().rollback();
+	}
+
+	public static void closeSession() {
+		HibUtil.getSession().close();
+	}
+
+	public static Session getSession() {
+		Session hibernateSession = sessionFactory.getCurrentSession();
+		return hibernateSession;
+	}
+}
