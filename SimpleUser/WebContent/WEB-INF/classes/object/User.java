98422465ffe82de9b352773eb46841a9e5a0c31b
Jason McKerr <mckerrj@gmail.com>
2013-02-02 19:47:26 -0500

    imports, suppress warnings

--- SimpleUser/WebContent/WEB-INF/classes/object/User.java
+++ SimpleUser/WebContent/WEB-INF/classes/object/User.java
@@ -6,14 +6,13 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name="user", schema="simpleapp")
 public class User implements Serializable {
