10ef3872c2709e9b688c1b9e1b465809a7aefdef
Jason McKerr <mckerrj@gmail.com>
2013-02-02 19:47:53 -0500

    import

--- SimpleUser/WebContent/WEB-INF/classes/DAO/hibernateDAO/HibUtil.java
+++ SimpleUser/WebContent/WEB-INF/classes/DAO/hibernateDAO/HibUtil.java
@@ -1,7 +1,6 @@
 package DAO.hibernateDAO;
 
 import org.hibernate.Session;
-import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
