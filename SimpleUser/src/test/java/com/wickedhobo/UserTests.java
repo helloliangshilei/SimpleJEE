e2915bf4d40846c45f84f5150c7b39aaccb45659
Jason McKerr <mckerrj@gmail.com>
2013-04-12 08:35:45 -0400

    Full working test set for methods in UserDAOHibSpringImpl.

--- SimpleUser/src/test/java/com/wickedhobo/UserTests.java
+++ SimpleUser/src/test/java/com/wickedhobo/UserTests.java
@@ -1,8 +1,12 @@
 package com.wickedhobo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
@@ -19,19 +23,19 @@ import com.wickedhobo.object.Role;
 import com.wickedhobo.object.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
 public class UserTests {
-	
+
 	private static Logger log = LoggerFactory.getLogger(UserTests.class);
-	
+
 	@Autowired
 	UserDAOHibSpringImpl userDAO;
-	
+
 	@Test
 	@Transactional()
-	public void testInsertUser() {
+	public void testUserSave() {
 		User user = new User();
-		
+
 		user.setFirstName("Anne");
 		user.setLastName("Halgren");
 		user.setUserName("halgrena");
@@ -41,23 +45,21 @@ public class UserTests {
 		Set<Role> roles = new HashSet<Role>();
 		roles.add(role);
 		user.setRoles(roles);
-		
+
 		userDAO.saveUser(user);
-		
+
 		User user2 = userDAO.findUserByUsername("halgrena");
-		
+
 		assertNotNull(user2);
 		assertEquals("halgrena", user2.getUserName());
-		log.debug("testInsertUser has passed all tests!");
+		log.debug("testUserSave has passed all tests!");
 	}
 
-	
 	@Test
 	@Transactional()
-	public void testFindUser() {
-		
+	public void testUserUpdatee() {
 		User user = new User();
-		
+
 		user.setFirstName("Anne");
 		user.setLastName("Halgren");
 		user.setUserName("halgrena");
@@ -67,14 +69,116 @@ public class UserTests {
 		Set<Role> roles = new HashSet<Role>();
 		roles.add(role);
 		user.setRoles(roles);
-		
+
 		userDAO.saveUser(user);
-		
+
 		User user2 = userDAO.findUserByUsername("halgrena");
-		
+
+		user2.setFirstName("Anne2");
+		user2.setLastName("Halgren2");
+		user2.setPassword("anne3142");
+
+		userDAO.updateUser(user2);
+
+		User user3 = userDAO.findUserByUsername("halgrena");
+
+		assertNotNull(user3);
+		assertEquals("Halgren2", user3.getLastName());
+		assertEquals("Anne2", user3.getFirstName());
+		log.debug("testUserUpdate has passed all tests!");
+	}
+
+	@Test
+	@Transactional()
+	public void testUserFind() {
+
+		User user = new User();
+
+		user.setFirstName("Anne");
+		user.setLastName("Halgren");
+		user.setUserName("halgrena");
+		user.setPassword("anne314");
+		Role role = new Role();
+		role.setRole("administrator");
+		Set<Role> roles = new HashSet<Role>();
+		roles.add(role);
+		user.setRoles(roles);
+
+		userDAO.saveUser(user);
+
+		User user2 = userDAO.findUserByUsername("halgrena");
+
 		assertEquals(1, user2.getRoles().size());
 		assertEquals("Halgren", user2.getLastName());
 		assertEquals("Anne", user2.getFirstName());
-		log.debug("testFindUser has passed all tests.");
+		log.debug("testUserFind has passed all tests.");
+	}
+
+	@Test
+	@Transactional()
+	public void testUserDelete() {
+		User user = new User();
+
+		user.setFirstName("Anne");
+		user.setLastName("Halgren");
+		user.setUserName("halgrena");
+		user.setPassword("anne314");
+		Role role = new Role();
+		role.setRole("administrator");
+		Set<Role> roles = new HashSet<Role>();
+		roles.add(role);
+		user.setRoles(roles);
+
+		userDAO.saveUser(user);
+
+		User user2 = userDAO.findUserByUsername("halgrena");
+		userDAO.deleteUser(user2);
+
+		User user3 = userDAO.findUserByUsername("halgrena");
+		assertNull(user3);
+	}
+
+	@Test
+	@Transactional()
+	public void testUserFindAll() {
+		List<User> userList = new ArrayList<User>();
+
+		userList = userDAO.listUsers();
+
+		for (User user : userList) {
+			log.debug("Last Name (Find All): " + user.getLastName());
+			for (Role role : user.getRoles()) {
+				log.debug("   (testUserfindAll) Roles List 1: " + role.getRole());
+			}
+		}
+		assertEquals(2, userList.size());
+		log.debug("testUserFindAll has passed all tests!");
+	}
+
+	@Test
+	@Transactional()
+	public void testUserFindByRole() {
+		List<User> userList = new ArrayList<User>();
+		List<User> userList2 = new ArrayList<User>();
+
+		userList = userDAO.listUsersByRoles("developer");
+		userList2 = userDAO.listUsersByRoles("administrator");
+
+		for (User user : userList) {
+			log.debug("(testUserFindByRole) Last Name List 1 findByRole: " + user.getLastName());
+			for (Role role : user.getRoles()) {
+				log.debug("   (testUserFindByRole)Roles List 1: " + role.getRole());
+			}
+		}
+
+		for (User user : userList2) {
+			log.debug("(testUserFindByRole) Last Name List 2 findByRole: " + user.getLastName());
+			for (Role role : user.getRoles()) {
+				log.debug("   (testUserFindByRole) Roles List 2: " + role.getRole());
+			}
+		}
+		assertEquals(1, userList.size());
+		assertEquals(2, userList2.size());
+		log.debug("testUserFindByRole has passed all tests!");
 	}
 }
