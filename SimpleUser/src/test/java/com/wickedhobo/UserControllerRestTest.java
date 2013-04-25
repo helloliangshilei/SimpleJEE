08d521119e9527cbc8697c4f89ff8dff5a2b3996
Jason McKerr <mckerrj@gmail.com>
2013-04-25 14:28:54 -0400

    testing whitespace issue with github

--- SimpleUser/src/test/java/com/wickedhobo/UserControllerRestTest.java
+++ SimpleUser/src/test/java/com/wickedhobo/UserControllerRestTest.java
@@ -11,7 +11,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
