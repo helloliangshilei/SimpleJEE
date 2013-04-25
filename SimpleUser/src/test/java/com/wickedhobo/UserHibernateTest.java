304c595c5be6fc6f9bda173a217c12c8e3239d23
Jason McKerr <mckerrj@gmail.com>
2013-04-25 09:12:28 -0400

    Changing names to follow Junit/Maven conventions

--- SimpleUser/src/test/java/com/wickedhobo/UserHibernateTest.java
+++ SimpleUser/src/test/java/com/wickedhobo/UserHibernateTest.java
@@ -5,34 +5,28 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+import org.hibernate.Session;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.wickedhobo.DAO.hibernateDAO.UserDAOHibSpringImpl;
-import com.wickedhobo.object.Role;
+import com.wickedhobo.DAO.hibernateDAO.HibUtil;
+import com.wickedhobo.DAO.hibernateDAO.UserDAO;
+import com.wickedhobo.DAO.hibernateDAO.UserDAOHibOnlyImpl;
 import com.wickedhobo.object.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })
-public class UserTestsSpringHibernate {
+public class UserHibernateTest {
 
-	private static Logger log = LoggerFactory.getLogger(UserTestsSpringHibernate.class);
-
-	@Autowired
-	UserDAOHibSpringImpl userDAO;
+	private static Logger log = LoggerFactory.getLogger(UserHibernateTest.class);
 
 	@Test
-	@Transactional()
 	public void testUserSave() {
 		User user = new User();
 
@@ -40,82 +34,82 @@ public class UserTestsSpringHibernate {
 		user.setLastName("Halgren");
 		user.setUserName("halgrena");
 		user.setPassword("anne314");
-		Role role = new Role();
-		role.setRole("administrator");
-		Set<Role> roles = new HashSet<Role>();
-		roles.add(role);
-		user.setRoles(roles);
 
+		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userDAO.saveUser(user);
 
-		User user2 = userDAO.findUserByUsername("halgrena");
+		// Now load without using DAO structure and compare
+		Session session = HibUtil.getSessionFactory().openSession();
+		HibUtil.beginTx();
+		User user2 = (User) session.get(User.class, user.getUserName());
+		HibUtil.commitTx();
+		HibUtil.closeSession();
 
 		assertNotNull(user2);
-		assertEquals("halgrena", user2.getUserName());
+		assertEquals("halgrena2", user2.getUserName());
 		log.debug("testUserSave has passed all tests!");
+
+		userDAO.deleteUser(user);
 	}
 
 	@Test
-	@Transactional()
-	public void testUserUpdatee() {
+	public void testUserUpdate() {
 		User user = new User();
 
 		user.setFirstName("Anne");
 		user.setLastName("Halgren");
 		user.setUserName("halgrena");
 		user.setPassword("anne314");
-		Role role = new Role();
-		role.setRole("administrator");
-		Set<Role> roles = new HashSet<Role>();
-		roles.add(role);
-		user.setRoles(roles);
 
+		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userDAO.saveUser(user);
 
-		User user2 = userDAO.findUserByUsername("halgrena");
+		User user2 = new User();
 
 		user2.setFirstName("Anne2");
 		user2.setLastName("Halgren2");
+		user2.setUserName("halgrena");
 		user2.setPassword("anne3142");
 
 		userDAO.updateUser(user2);
 
-		User user3 = userDAO.findUserByUsername("halgrena");
+		// Now load without using DAO structure and compare
+		Session session = HibUtil.getSessionFactory().openSession();
+		HibUtil.beginTx();
+		User user3 = (User) session.get(User.class, user.getUserName());
+		HibUtil.commitTx();
+		HibUtil.closeSession();
 
 		assertNotNull(user3);
-		assertEquals("Halgren2", user3.getLastName());
-		assertEquals("Anne2", user3.getFirstName());
+		assertEquals("Halgren2", user2.getLastName());
+		assertEquals("Anne2", user2.getFirstName());
 		log.debug("testUserUpdate has passed all tests!");
+
+		userDAO.deleteUser(user);
 	}
 
 	@Test
-	@Transactional()
 	public void testUserFind() {
-
 		User user = new User();
 
 		user.setFirstName("Anne");
 		user.setLastName("Halgren");
 		user.setUserName("halgrena");
 		user.setPassword("anne314");
-		Role role = new Role();
-		role.setRole("administrator");
-		Set<Role> roles = new HashSet<Role>();
-		roles.add(role);
-		user.setRoles(roles);
 
+		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userDAO.saveUser(user);
 
 		User user2 = userDAO.findUserByUsername("halgrena");
 
-		assertEquals(1, user2.getRoles().size());
 		assertEquals("Halgren", user2.getLastName());
 		assertEquals("Anne", user2.getFirstName());
-		log.debug("testUserFind has passed all tests.");
+		log.debug("testUserFind has passed all tests!");
+
+		userDAO.deleteUser(user);
 	}
 
 	@Test
-	@Transactional()
 	public void testUserDelete() {
 		User user = new User();
 
@@ -123,62 +117,45 @@ public class UserTestsSpringHibernate {
 		user.setLastName("Halgren");
 		user.setUserName("halgrena");
 		user.setPassword("anne314");
-		Role role = new Role();
-		role.setRole("administrator");
-		Set<Role> roles = new HashSet<Role>();
-		roles.add(role);
-		user.setRoles(roles);
 
+		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userDAO.saveUser(user);
 
-		User user2 = userDAO.findUserByUsername("halgrena");
-		userDAO.deleteUser(user2);
-		
-		User user3 = userDAO.findUserByUsername("halgrena");
-		
-		assertNull(user3);
+		userDAO.deleteUser(user);
+
+		// Now load without using DAO structure and compare
+		Session session = HibUtil.getSessionFactory().openSession();
+		HibUtil.beginTx();
+		User user2 = (User) session.get(User.class, "halgrena");
+		HibUtil.commitTx();
+		HibUtil.closeSession();
+
+		assertNull(user2);
 		log.debug("testUserDelete has passed all tests!");
 	}
 
 	@Test
-	@Transactional()
+	//Sort of a bad test since it relies in state of DB from initial import, but it does what I want.
 	public void testUserFindAll() {
 		List<User> userList = new ArrayList<User>();
 
+		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userList = userDAO.listUsers();
 
-		for (User user : userList) {
-			log.debug("Last Name (Find All): " + user.getLastName());
-			for (Role role : user.getRoles()) {
-				log.debug("   (testUserfindAll) Roles List 1: " + role.getRole());
-			}
-		}
 		assertEquals(2, userList.size());
 		log.debug("testUserFindAll has passed all tests!");
 	}
 
 	@Test
-	@Transactional()
+	//Sort of a bad test since it relies in state of DB from initial import, but it does what I want.
 	public void testUserFindByRole() {
 		List<User> userList = new ArrayList<User>();
 		List<User> userList2 = new ArrayList<User>();
 
+		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userList = userDAO.listUsersByRoles("developer");
 		userList2 = userDAO.listUsersByRoles("administrator");
 
-		for (User user : userList) {
-			log.debug("(testUserFindByRole) Last Name List 1 findByRole: " + user.getLastName());
-			for (Role role : user.getRoles()) {
-				log.debug("   (testUserFindByRole)Roles List 1: " + role.getRole());
-			}
-		}
-
-		for (User user : userList2) {
-			log.debug("(testUserFindByRole) Last Name List 2 findByRole: " + user.getLastName());
-			for (Role role : user.getRoles()) {
-				log.debug("   (testUserFindByRole) Roles List 2: " + role.getRole());
-			}
-		}
 		assertEquals(1, userList.size());
 		assertEquals(2, userList2.size());
 		log.debug("testUserFindByRole has passed all tests!");
--- SimpleUser/src/test/java/com/wickedhobo/UserSpringHibernateTest.java
+++ SimpleUser/src/test/java/com/wickedhobo/UserSpringHibernateTest.java
@@ -5,28 +5,34 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import org.hibernate.Session;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.wickedhobo.DAO.hibernateDAO.HibUtil;
-import com.wickedhobo.DAO.hibernateDAO.UserDAO;
-import com.wickedhobo.DAO.hibernateDAO.UserDAOHibOnlyImpl;
+import com.wickedhobo.DAO.hibernateDAO.UserDAOHibSpringImpl;
+import com.wickedhobo.object.Role;
 import com.wickedhobo.object.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })
-public class UserTestsHibernate {
+public class UserSpringHibernateTest {
 
-	private static Logger log = LoggerFactory.getLogger(UserTestsHibernate.class);
+	private static Logger log = LoggerFactory.getLogger(UserSpringHibernateTest.class);
+
+	@Autowired
+	UserDAOHibSpringImpl userDAO;
 
 	@Test
+	@Transactional()
 	public void testUserSave() {
 		User user = new User();
 
@@ -34,82 +40,82 @@ public class UserTestsHibernate {
 		user.setLastName("Halgren");
 		user.setUserName("halgrena");
 		user.setPassword("anne314");
+		Role role = new Role();
+		role.setRole("administrator");
+		Set<Role> roles = new HashSet<Role>();
+		roles.add(role);
+		user.setRoles(roles);
 
-		UserDAO userDAO = new UserDAOHibOnlyImpl();
-		userDAO.saveUser(user);
-
-		// Now load without using DAO structure and compare
-		Session session = HibUtil.getSessionFactory().openSession();
-		HibUtil.beginTx();
-		User user2 = (User) session.get(User.class, user.getUserName());
-		HibUtil.commitTx();
-		HibUtil.closeSession();
-
-		assertNotNull(user2);
-		assertEquals("halgrena2", user2.getUserName());
-		log.debug("testUserSave has passed all tests!");
-
-		userDAO.deleteUser(user);
-	}
-
-	@Test
-	public void testUserUpdate() {
-		User user = new User();
-
-		user.setFirstName("Anne");
-		user.setLastName("Halgren");
-		user.setUserName("halgrena");
-		user.setPassword("anne314");
-
-		UserDAO userDAO = new UserDAOHibOnlyImpl();
-		userDAO.saveUser(user);
-
-		User user2 = new User();
-
-		user2.setFirstName("Anne2");
-		user2.setLastName("Halgren2");
-		user2.setUserName("halgrena");
-		user2.setPassword("anne3142");
-
-		userDAO.updateUser(user2);
-
-		// Now load without using DAO structure and compare
-		Session session = HibUtil.getSessionFactory().openSession();
-		HibUtil.beginTx();
-		User user3 = (User) session.get(User.class, user.getUserName());
-		HibUtil.commitTx();
-		HibUtil.closeSession();
-
-		assertNotNull(user3);
-		assertEquals("Halgren2", user2.getLastName());
-		assertEquals("Anne2", user2.getFirstName());
-		log.debug("testUserUpdate has passed all tests!");
-
-		userDAO.deleteUser(user);
-	}
-
-	@Test
-	public void testUserFind() {
-		User user = new User();
-
-		user.setFirstName("Anne");
-		user.setLastName("Halgren");
-		user.setUserName("halgrena");
-		user.setPassword("anne314");
-
-		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userDAO.saveUser(user);
 
 		User user2 = userDAO.findUserByUsername("halgrena");
 
-		assertEquals("Halgren", user2.getLastName());
-		assertEquals("Anne", user2.getFirstName());
-		log.debug("testUserFind has passed all tests!");
-
-		userDAO.deleteUser(user);
+		assertNotNull(user2);
+		assertEquals("halgrena", user2.getUserName());
+		log.debug("testUserSave has passed all tests!");
 	}
 
 	@Test
+	@Transactional()
+	public void testUserUpdatee() {
+		User user = new User();
+
+		user.setFirstName("Anne");
+		user.setLastName("Halgren");
+		user.setUserName("halgrena");
+		user.setPassword("anne314");
+		Role role = new Role();
+		role.setRole("administrator");
+		Set<Role> roles = new HashSet<Role>();
+		roles.add(role);
+		user.setRoles(roles);
+
+		userDAO.saveUser(user);
+
+		User user2 = userDAO.findUserByUsername("halgrena");
+
+		user2.setFirstName("Anne2");
+		user2.setLastName("Halgren2");
+		user2.setPassword("anne3142");
+
+		userDAO.updateUser(user2);
+
+		User user3 = userDAO.findUserByUsername("halgrena");
+
+		assertNotNull(user3);
+		assertEquals("Halgren2", user3.getLastName());
+		assertEquals("Anne2", user3.getFirstName());
+		log.debug("testUserUpdate has passed all tests!");
+	}
+
+	@Test
+	@Transactional()
+	public void testUserFind() {
+
+		User user = new User();
+
+		user.setFirstName("Anne");
+		user.setLastName("Halgren");
+		user.setUserName("halgrena");
+		user.setPassword("anne314");
+		Role role = new Role();
+		role.setRole("administrator");
+		Set<Role> roles = new HashSet<Role>();
+		roles.add(role);
+		user.setRoles(roles);
+
+		userDAO.saveUser(user);
+
+		User user2 = userDAO.findUserByUsername("halgrena");
+
+		assertEquals(1, user2.getRoles().size());
+		assertEquals("Halgren", user2.getLastName());
+		assertEquals("Anne", user2.getFirstName());
+		log.debug("testUserFind has passed all tests.");
+	}
+
+	@Test
+	@Transactional()
 	public void testUserDelete() {
 		User user = new User();
 
@@ -117,45 +123,62 @@ public class UserTestsHibernate {
 		user.setLastName("Halgren");
 		user.setUserName("halgrena");
 		user.setPassword("anne314");
+		Role role = new Role();
+		role.setRole("administrator");
+		Set<Role> roles = new HashSet<Role>();
+		roles.add(role);
+		user.setRoles(roles);
 
-		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userDAO.saveUser(user);
 
-		userDAO.deleteUser(user);
-
-		// Now load without using DAO structure and compare
-		Session session = HibUtil.getSessionFactory().openSession();
-		HibUtil.beginTx();
-		User user2 = (User) session.get(User.class, "halgrena");
-		HibUtil.commitTx();
-		HibUtil.closeSession();
-
-		assertNull(user2);
+		User user2 = userDAO.findUserByUsername("halgrena");
+		userDAO.deleteUser(user2);
+		
+		User user3 = userDAO.findUserByUsername("halgrena");
+		
+		assertNull(user3);
 		log.debug("testUserDelete has passed all tests!");
 	}
 
 	@Test
-	//Sort of a bad test since it relies in state of DB from initial import, but it does what I want.
+	@Transactional()
 	public void testUserFindAll() {
 		List<User> userList = new ArrayList<User>();
 
-		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userList = userDAO.listUsers();
 
+		for (User user : userList) {
+			log.debug("Last Name (Find All): " + user.getLastName());
+			for (Role role : user.getRoles()) {
+				log.debug("   (testUserfindAll) Roles List 1: " + role.getRole());
+			}
+		}
 		assertEquals(2, userList.size());
 		log.debug("testUserFindAll has passed all tests!");
 	}
 
 	@Test
-	//Sort of a bad test since it relies in state of DB from initial import, but it does what I want.
+	@Transactional()
 	public void testUserFindByRole() {
 		List<User> userList = new ArrayList<User>();
 		List<User> userList2 = new ArrayList<User>();
 
-		UserDAO userDAO = new UserDAOHibOnlyImpl();
 		userList = userDAO.listUsersByRoles("developer");
 		userList2 = userDAO.listUsersByRoles("administrator");
 
+		for (User user : userList) {
+			log.debug("(testUserFindByRole) Last Name List 1 findByRole: " + user.getLastName());
+			for (Role role : user.getRoles()) {
+				log.debug("   (testUserFindByRole)Roles List 1: " + role.getRole());
+			}
+		}
+
+		for (User user : userList2) {
+			log.debug("(testUserFindByRole) Last Name List 2 findByRole: " + user.getLastName());
+			for (Role role : user.getRoles()) {
+				log.debug("   (testUserFindByRole) Roles List 2: " + role.getRole());
+			}
+		}
 		assertEquals(1, userList.size());
 		assertEquals(2, userList2.size());
 		log.debug("testUserFindByRole has passed all tests!");
