ea7c0d86fd9ee0235a7b07a0f2569965f9e823c3
mckerrj <mckerrj@gmail.com>
2013-05-02 14:57:22 -0400

    formatting, cleanup
    Signed-off-by: mckerrj <mckerrj@gmail.com>

--- SimpleUser/src/main/java/com/wickedhobo/object/User.java
+++ SimpleUser/src/main/java/com/wickedhobo/object/User.java
@@ -13,88 +13,88 @@ import javax.persistence.Table;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 
-import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonManagedReference;
 import org.hibernate.annotations.NamedQueries;
 import org.hibernate.annotations.NamedQuery;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(name="user", schema="simpleapp")
+@Table(name = "user", schema = "simpleapp")
 @NamedQueries({
-	@NamedQuery(name="object.user.listUsersByRole",
-	query="from User as user inner join fetch user.roles as role where role.role = :role"
-	)
+    @NamedQuery(name = "object.user.listUsersByRole",
+                query = "from User as user inner join fetch user.roles as role where role.role = :role"
+    )
 })
 public class User implements Serializable {
 
-	@Id 
-	@Column(name="username", nullable=false, length=100)
-	private String userName = null;
-	
-	@Column(name="firstName", nullable=false, length=100)
-	private String firstName = null;
-	
-	@Column(name="lastName", nullable=false, length=100)
-	private String lastName = null;
-	
-	@Column(name="password", nullable=false, length=100)
-	private String password = null;
-	
-	@ManyToMany(fetch=FetchType.LAZY)
-	@JsonManagedReference //Need this to prevent infinite loading of bidirectional relationship
-	//@Cascade(CascadeType.ALL)
+  @Id
+  @Column(name = "username", nullable = false, length = 100)
+  private String userName = null;
+
+  @Column(name = "firstName", nullable = false, length = 100)
+  private String firstName = null;
+
+  @Column(name = "lastName", nullable = false, length = 100)
+  private String lastName = null;
+
+  @Column(name = "password", nullable = false, length = 100)
+  private String password = null;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JsonManagedReference
+  // Need this to prevent infinite loading of bidirectional relationship
+  // @Cascade(CascadeType.ALL)
   @JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "user_username") }, inverseJoinColumns = { @JoinColumn(name = "role_role") })
-	private Set<Role> roles = new HashSet<Role>(0);
+  private Set<Role> roles = new HashSet<Role>(0);
 
-	public User() {
-	}
-	
-	public User(String userName, String firstName, String lastName,
-			String password) {
-		this.userName = userName;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.password = password;
-	}
-	
-	public String getUserName() {
-		return this.userName;
-	}
+  public User() {
+  }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+  public User(String userName, String firstName, String lastName,
+      String password) {
+    this.userName = userName;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.password = password;
+  }
 
-	public String getFirstName() {
-		return firstName;
-	}
+  public String getUserName() {
+    return this.userName;
+  }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
 
-	public String getLastName() {
-		return lastName;
-	}
+  public String getFirstName() {
+    return firstName;
+  }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-	public String getPassword() {
-		return password;
-	}
+  public String getLastName() {
+    return lastName;
+  }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-	public Set<Role> getRoles() {
-		return this.roles;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Set<Role> getRoles() {
+    return this.roles;
+  }
+
+  public void setRoles(Set<Role> roles) {
+    this.roles = roles;
+  }
 }
