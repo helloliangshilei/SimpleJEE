e77c45ff3be3342d29802b657dfa548219306012
mckerrj <mckerrj@gmail.com>
2013-05-02 14:58:22 -0400

    formatting, imports cleanup
    Signed-off-by: mckerrj <mckerrj@gmail.com>

--- SimpleUser/src/main/java/com/wickedhobo/rest/UserController.java
+++ SimpleUser/src/main/java/com/wickedhobo/rest/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wickedhobo.DAO.hibernateDAO.UserDAOHibSpringImpl;
@@ -100,7 +99,6 @@ public class UserController {
 
     List<User> userList = new ArrayList<User>();
     userList = userDAO.listUsersByRoles(roleName);
-
     return userList;
   }
 }
