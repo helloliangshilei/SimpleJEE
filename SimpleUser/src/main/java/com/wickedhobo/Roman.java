ff6f6f76475cbcd8960a4a7559c5a149a4f17f08
mckerrj <mckerrj@gmail.com>
2013-05-30 15:31:02 -0400

    Cleanup, add TODO's for Ryan, create identical stub method for ryan, add tests for stubmethod.
    Signed-off-by: mckerrj <mckerrj@gmail.com>

--- SimpleUser/src/main/java/com/wickedhobo/Roman.java
+++ SimpleUser/src/main/java/com/wickedhobo/Roman.java
@@ -1,7 +1,6 @@
 package com.wickedhobo;
 
 import java.util.HashMap;
-import java.util.StringTokenizer;
 
 public class Roman {
   
@@ -17,9 +16,9 @@ public class Roman {
     romansMap.put("I", 1);
   }
   
-  public Integer returnNumbers(String romansIn) {
+  public Integer returnNumbersBackwards(String romansIn) {
     int lastPosition = 0;
-    Integer runningValue = 0; //is this ok since there's no zero? Or should it basically be null, and no number assigned?
+    Integer runningValue = 0; 
     
     try {
       for(int i = romansIn.length() - 1; i >= 0 ; i--){
@@ -41,4 +40,33 @@ public class Roman {
     }
     return runningValue;
   }
+  
+//TODO: Fix the Zero problem below.  Test for null.
+//TODO: Switch the string indexing from backwards to forwards and fix.
+  public Integer returnNumbersForwards(String romansIn) {
+    int lastPosition = 0;
+    Integer runningValue = 0; //is this ok since there's no zero? Or should it basically be null, and no number assigned?
+    
+    try {
+      for(int i = romansIn.length() - 1; i >= 0 ; i--){ //change this to the other direction, fix method to work.
+        char c = romansIn.toUpperCase().charAt(i);
+        int charVal = romansMap.get(Character.toString(c));
+        
+        if (lastPosition <= charVal) {
+          runningValue += charVal;
+          lastPosition = charVal;
+        }
+        else {
+          runningValue -= charVal;
+          lastPosition = charVal;
+        }
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    return runningValue;
+  }
+  
+  
 }
--- SimpleUser/src/test/java/com/wickedhobo/RomanTest.java
+++ SimpleUser/src/test/java/com/wickedhobo/RomanTest.java
@@ -14,18 +14,17 @@ public class RomanTest {
   @Test
   public void testRomans() throws Exception {
     Roman roman = new Roman();
-    //Integer number = roman.returnNumbers("VIII");
-    System.out.println("The Roman Numberal VIII(8) is " + roman.returnNumbers("VIII").toString());
-    System.out.println("The Roman Numberal III(3) is " + roman.returnNumbers("III").toString());
-    System.out.println("The Roman Numberal IV(4) is " + roman.returnNumbers("IV").toString());
-    System.out.println("The Roman Numberal IIII(4) is " + roman.returnNumbers("IIII").toString());
-    System.out.println("The Roman Numberal XIII(13) is " + roman.returnNumbers("XIII").toString());
-    System.out.println("The Roman Numberal XVII(17) is " + roman.returnNumbers("XVII").toString());
-    System.out.println("The Roman Numberal XL(40) is " + roman.returnNumbers("XL").toString());
-    System.out.println("The Roman Numberal CVI(106) is " + roman.returnNumbers("CVI").toString());
-    System.out.println("The Roman Numberal DCCLXXXIX(789) is " + roman.returnNumbers("DCCLXXXIX").toString());
-    System.out.println("The Roman Numberal MV(1005) is " + roman.returnNumbers("MV").toString());
-    System.out.println("The Roman Numberal MMCCCXLVI(2436) is " + roman.returnNumbers("MMCCCXLVI").toString());
-    System.out.println("The Roman Numberal CMMMV(2905) is " + roman.returnNumbers("CMMMV").toString());
+    System.out.println("The Roman Numberal VIII(8) is " + roman.returnNumbersBackwards("VIII").toString());
+    System.out.println("The Roman Numberal III(3) is " + roman.returnNumbersBackwards("III").toString());
+    System.out.println("The Roman Numberal IV(4) is " + roman.returnNumbersBackwards("IV").toString());
+    System.out.println("The Roman Numberal IIII(4) is " + roman.returnNumbersBackwards("IIII").toString());
+    System.out.println("The Roman Numberal XIII(13) is " + roman.returnNumbersBackwards("XIII").toString());
+    System.out.println("The Roman Numberal XVII(17) is " + roman.returnNumbersBackwards("XVII").toString());
+    System.out.println("The Roman Numberal XL(40) is " + roman.returnNumbersBackwards("XL").toString());
+    System.out.println("The Roman Numberal CVI(106) is " + roman.returnNumbersBackwards("CVI").toString());
+    System.out.println("The Roman Numberal DCCLXXXIX(789) is " + roman.returnNumbersBackwards("DCCLXXXIX").toString());
+    System.out.println("The Roman Numberal MV(1005) is " + roman.returnNumbersBackwards("MV").toString());
+    System.out.println("The Roman Numberal MMCCCXLVI(2436) is " + roman.returnNumbersBackwards("MMCCCXLVI").toString());
+    System.out.println("The Roman Numberal CMMMV(2905) is " + roman.returnNumbersBackwards("CMMMV").toString());
   }
 }
