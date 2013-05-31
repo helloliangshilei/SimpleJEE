3ec8fc17d1289ff9fcec3522fc1c9ea3f530c69f
mckerrj <mckerrj@gmail.com>
2013-05-31 13:07:46 -0400

    Signed-off-by: mckerrj <mckerrj@gmail.com>

--- SimpleUser/src/main/java/com/wickedhobo/Roman.java
+++ SimpleUser/src/main/java/com/wickedhobo/Roman.java
@@ -3,9 +3,9 @@ package com.wickedhobo;
 import java.util.HashMap;
 
 public class Roman {
-  
+
   private HashMap<String, Integer> romansMap = new HashMap<String, Integer>();
-  
+
   public Roman() {
     romansMap.put("M", 1000);
     romansMap.put("D", 500);
@@ -15,43 +15,50 @@ public class Roman {
     romansMap.put("V", 5);
     romansMap.put("I", 1);
   }
-  
+
+  /*
+   * Notes for Ryan: 
+   * Remember length/iteration starts at zero 
+   * i++ 
+   * understand for loop logic/boolean: initialize, boolean logic/termination, increment
+   * +=/-= syntax 
+   * why toUpperCase 
+   * Understand hashmap behavior
+   */
   public Integer returnNumbersBackwards(String romansIn) {
     int lastPosition = 0;
-    Integer runningValue = 0; 
-    
-    try {
-      for(int i = romansIn.length() - 1; i >= 0 ; i--){
-        char c = romansIn.toUpperCase().charAt(i);
-        int charVal = romansMap.get(Character.toString(c));
-        
-        if (lastPosition <= charVal) {
-          runningValue += charVal;
-          lastPosition = charVal;
-        }
-        else {
-          runningValue -= charVal;
-          lastPosition = charVal;
-        }
+    Integer runningValue = 0;
+
+    for (int i = romansIn.length() - 1; i >= 0; i--) {
+      char c = romansIn.toUpperCase().charAt(i);
+      int charVal = romansMap.get(Character.toString(c));
+
+      if (lastPosition <= charVal) {
+        runningValue += charVal;
+        lastPosition = charVal;
       }
-    }
-    catch (Exception e) {
-      e.printStackTrace();
+      else {
+        runningValue -= charVal;
+        lastPosition = charVal;
+      }
     }
     return runningValue;
   }
-  
-//TODO: Fix the Zero problem below.  Test for null.
-//TODO: Switch the string indexing from backwards to forwards and fix.
+
+  // TODO: Fix the Zero problem below. Test for null.
+  // TODO: Switch the string indexing from backwards to forwards and fix.
   public Integer returnNumbersForwards(String romansIn) {
     int lastPosition = 0;
-    Integer runningValue = 0; //is this ok since there's no zero? Or should it basically be null, and no number assigned?
-    
+    Integer runningValue = 0; // is this ok since there's no zero? Or should it
+                              // basically be null, and no number assigned?
+
     try {
-      for(int i = romansIn.length() - 1; i >= 0 ; i--){ //change this to the other direction, fix method to work.
+      for (int i = romansIn.length() - 1; i >= 0; i--) { // change this to the
+                                                         // other direction, fix
+                                                         // method to work.
         char c = romansIn.toUpperCase().charAt(i);
         int charVal = romansMap.get(Character.toString(c));
-        
+
         if (lastPosition <= charVal) {
           runningValue += charVal;
           lastPosition = charVal;
