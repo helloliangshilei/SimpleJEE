d3da3434821daa17762dade186ead284ce454c57
mckerrj <mckerrj@gmail.com>
2014-03-09 17:56:02 -0400

    cleaning up.

--- SimpleUser/src/main/java/com/wickedhobo/Roman.java
+++ SimpleUser/src/main/java/com/wickedhobo/Roman.java
@@ -46,38 +46,4 @@ public class Roman {
     }
     return runningValue;
   }
-
-  // TODO: Fix the Zero problem below. Test for null.
-  // TODO: Switch the string indexing from backwards to forwards and fix.
-  public Integer returnNumbersForwards(String romansIn) {
-    int lastPosition = 0;
-    Integer runningValue = 0; // is this ok since there's no zero? Or should it
-                              // basically be null, and no number assigned?
-    							//A: the hashmap class allows for null value and the null key. 
-    							//I added 'romansMap.put("", 0)' to the hashmap
-    							//Also I think that runningValue = 0 is denoting that at LP=0 the running value is 0 
-//so the problem with going forward is that LP^0 can either be a + or - value, where as going backwards the last roman numeral could only be positive
-    try {
-      for (int i=0; i <= romansIn.length()-1; i++) { // change this to the
-                                                         // other direction, fix
-                                                         // method to work.
-        char c = romansIn.toUpperCase().charAt(i);
-        System.out.println("C is: " + c);
-        /*int charVal = romansMap.get(Character.toString(c));
-
-        if (lastPosition <= charVal) {
-          runningValue -= charVal;
-          lastPosition = charVal;
-        }
-        else {
-          runningValue += charVal;
-          lastPosition = charVal;
-        }*/
-      }
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
-    return runningValue;
-  }
 }
